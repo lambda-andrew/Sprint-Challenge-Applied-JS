92418215764972f4d1c2c9d92e0f3b28107c38ae
Eric Nunn <lowlyherotech@gmail.com>
2019-04-19 13:03:09 -0400

    Finished Tabs.js. Deleted comments for code readability

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,17 +1,15 @@
 class TabLink {
   constructor(tabElement){
     this.tabElement = tabElement;
-    // Get the `data-tab` value from this.tabElement and store it here
     this.tabData = this.tabElement.dataset.tab; 
-    // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
-    // // Check to see if this.tabData is equal to 'all'
+
      if(this.tabData == 'all'){
-       this.cards = document.querySelectorAll(`[data-tab='${this.tabData}']`);
+       this.cards = document.querySelectorAll('.card');
      } else {
-       this.cards = this.tabData;
+      this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
      }
-     // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    // this.cards = Array.from(this.cards).map(new TabCard(this.cards));
+
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
     
     this.tabElement.addEventListener('click', () => {
       this.selectTab();
@@ -21,14 +19,13 @@ class TabLink {
   selectTab(){
     const tabs = document.querySelectorAll('.tab');
     tabs.forEach(tab => {
-      tab.classList.remove('.active-tab')
+      tab.classList.remove('active-tab')
     });
     const cards = document.querySelectorAll('.card')
-    // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach(card => {
-      card.classList.style.display = 'none';
-    })
-    this.tabElement.classList.add('.active-tab');
+      card.style.display = 'none';
+    });
+    this.tabElement.classList.add('active-tab');
     this.cards.forEach(card => card.selectCard());
   }
 }
